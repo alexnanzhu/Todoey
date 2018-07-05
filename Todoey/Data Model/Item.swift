54d7c2092710a76aecaadcb7f4ed30ab2c759411
Alex Z <antonyzhu95@icloud.com>
2018-07-05 07:35:02 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  Todoey
+//
+//  Created by Alex Z on 7/4/18.
+//  Copyright © 2018 Alex Nan Zhu. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+
+}
